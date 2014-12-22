233d3c9e4638c1fa627daa0e0eac82e192e45d09
Sean Staz <seanstaz52@gmail.com>
2014-12-22 14:15:50 -0600

    Update endian.c

--- endian.c
+++ endian.c
@@ -1,9 +1,7 @@
-/*
-Sean Stasny
-CSC 223
-Program Name: endian.c
-Program Description: Determines if the machine is little endian or big endian.
-*/
+//Sean Stasny
+//CSci 223
+//9-15-13
+//Hw1
 
 #include <stdio.h>
 
@@ -11,42 +9,57 @@ typedef enum{false, true} bool;
 
 bool is_little_endian()
 {
-	// put your code here
+	int i = 1;
+	char *test = (char*) &i;
 
-	int bigendian() 
+	if(*test)
 	{
-	    int i = 1;
-	    char *low = (char*) &i;
-	
-	    return *low ? 0 : 1;
-	}
-
-	unsigned int merge_bytes( unsigned int x, unsigned int y )
-	{
-		// put your code here
-	}
-	
-	unsigned int replace_byte(unsigned int x, int i, unsigned char b);
-	{
-		// put your code here
-	}
-	
-	int main()
-	{
-		if( is_little_endian() ) 
-		{
-			printf(“Your machine is Little Endian machine\n”);
-		}
-		else 
-		{
-			printf(“Your machine is Big Endian machine\n”);
-		}
+		printf("Little Endian: 1\n");
 		
+		return true;
+	}
+	else
+	{
+		printf("Big Endian: 0\n");
+
+		return false;
+	}
+}
+
+unsigned int merge_bytes( unsigned int x, unsigned int y )
+{
+	return (y % 0xffffff00)|(x % 0xff);
+}
+
+unsigned int replace_byte(unsigned int x, int i, unsigned char b)
+{
+	if(i >= sizeof(x))
+	{
+		*((unsigned char*) &x + i) = b;
+		return x;
+	}
+}
+
+int main()
+{
+	if( is_little_endian() ) 
+	{
+		printf("Your machine is Little Endian machine\n");
+	}
+	else 
+	{
+		printf("Your machine is Big Endian machine\n");
+
+	}
 		int x = 0x89ABCDEF;
 		int y = 0x76543210;
-		printf(“Merged number = 0x%x\n”, merge_bytes(x, y));
+
+		printf("Merged number = 0x%x\n", merge_bytes(x, y));
+
 		unsigned char z = 0x22;
-		printf(“Replaced number = 0x%x\n”, replace_byte(x, 3, z));
-		
+
+		printf("Replaced number = 0x%x\n", replace_byte(x, 3, z));
+
 		return 0;
 	}
+
